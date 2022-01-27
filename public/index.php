74f15687b142a6e3186dd3dbfc45ff2060b11d09
Daniel Damasio <damasio.dan7@gmail.com>
2022-01-27 08:06:11 -0300

    Test view

--- public/index.php
+++ public/index.php
@@ -1,19 +1,13 @@
 <?php
 
 use Slim\Factory\AppFactory;
-use Psr\Http\Message\ResponseInterface as Response;
-use Psr\Http\Message\ServerRequestInterface as Request;
-use Slim\Views\PhpRenderer;
-use Slim\App;
 
 require __DIR__ . '/../vendor/autoload.php';
 
 $app = AppFactory::create();
 
-$app->get('/', function (Request $request, Response $response, $args) {
-
-    $renderer = new PhpRenderer('../app/views');
-    return $renderer->render($response, "home.php", $args);
-});
+// Register routes
+$routes = require __DIR__ . '/../config/routes.php';
+$routes($app);
 
 $app->run();
