b0eeeaf53e7d892a6171203ac45f414ea708cfaa
Daniel Damasio <damasio.dan7@gmail.com>
2022-01-27 07:58:10 -0300

    Test view, php render

--- config/routes.php
+++ config/routes.php
@@ -2,13 +2,15 @@
 
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
+use Slim\Views\PhpRenderer;
 use Slim\App;
 
 return function (App $app) {
 
     $app->get('/', function (Request $request, Response $response, $args) {
-        $response->getBody()->write("Hello world!");
-        return $response;
+
+        $renderer = new PhpRenderer('../app/views');
+        return $renderer->render($response, "home.php", $args);
     });
 
 };
